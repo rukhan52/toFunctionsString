eb53bf1faa08e58b0bb9b51d9b4201f616275ce4
Zerometer <zerometer03@gmail.com>
2022-03-13 10:41:47 +0500

    second commit

--- dart.dart
+++ dart.dart
@@ -1,35 +1,25 @@
+import 'dart:ffi';
+
 void main() {
-  String title = "Dart course";
-  print(title.toLowerCase());
-  print(title.toUpperCase());
-  print(title.codeUnits);
-  print(title.hashCode);
-  print(title.isEmpty);
-  print(title.isNotEmpty);
-  print(title.length);
-  print(title.runes);
-  print(title.runtimeType);
-  print(title.allMatches("Dart course"));
-  print(title.codeUnitAt(3));
-  print(title.compareTo('Dart course'));
-  print(title.contains('D'));
-  print(title.endsWith('urse'));
-  print(title.indexOf('art'));
-  print(title.padRight(4, 'r'));
-  print(title.lastIndexOf("e"));
-  print(title.matchAsPrefix('course'));
-  print(title.padLeft(3, 'D'));
-  print(title.replaceAll("Dart course", "Dart programming"));
-  print(title.replaceAllMapped("Dart course", (c) => 'couse'));
-  print(title.replaceFirst("rse", "sre"));
-  print(title.replaceFirstMapped("Dart", (D) => 'Dart'));
-  print(title.replaceRange(8, null, 'programming series'));
-  print(title.split('cousre'));
-  print(title.splitMapJoin("dartcourse"));
-  print(title.startsWith('Dar'));
-  print(title.substring(8));
-  print(title.trim());
-  print(title.trimLeft());
-  print(title.trimRight());
-  print(title);
+  int num1 = 123;
+  double num2 = 4;
+  String name = '1234';
+
+  // conversion double to int and vice versa
+  double num3 = num1.toDouble();
+  print('the value of int type goes parse $num3');
+  int num4 = num2.toInt();
+  print('the value of double type goes parse $num4');
+
+  // conversion String to int and vice versa
+  String name2 = num2.toString();
+  print('the value of int type goes parse $name2');
+  int num5 = int.parse(name);
+  print('the value of String type goes parse $num5');
+
+  //conversion double to string andvice versa
+  String name3 = num2.toString();
+  print('the value of double type goes parse $name3');
+  double num6 = double.parse(name);
+  print('the value of String type goes parse $num6');
 }
